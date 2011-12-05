19d2ded21216548b05f8451dae37abb14c72399c
Amir Barylko <amir@barylko.com>
2011-12-04 22:51:31 -0600

    A bit of refactoring

--- src/main/scala/ScoreCalculator.scala
+++ src/main/scala/ScoreCalculator.scala
@@ -7,24 +7,22 @@ trait ScoreCalculator {
 class DefaultScoreCalculator extends ScoreCalculator {
 	
 	def calculate(rolls: Iterable[Int]) : Int = {
-		val f = frames(rolls.toList) 
-		f reduce(_+_)
+		frames(rolls.toList) reduce(_+_)
 	}
 	
 	private def frames(rolls: List[Int]) : List[Int] = rolls match {
-		case 10 :: 10 :: 10 :: Nil => 30 :: Nil
-		case 10 :: second :: third :: tail => strike(second, third, tail)
-		case first :: second :: third :: tail => check_spare(first, second, third, tail)
-		case first :: second :: Nil => (first + second) :: Nil 
-		case first :: Nil => first :: Nil
+		case first :: second :: Nil => (first + second) :: Nil
+		case first :: second :: third :: Nil => (first + second + third) :: Nil
+		case first :: second :: third :: tail => strike_check(first, second, third, tail)
 		case Nil => Nil
 	}
 
-	private def strike(a: Int, b: Int, tail: List[Int]) : List[Int] = {
-		(10 + a + b) :: frames(a :: b :: tail.toList)
+	private def strike_check(a: Int, b: Int, c: Int, tail: List[Int]) : List[Int] = a match {
+		case 10 => (10 + b + c) :: frames(b :: c :: tail)
+		case _  => spare_check(a, b, c, tail)
 	}
 	
-	private def check_spare(a: Int, b: Int, c: Int, tail: List[Int]) : List[Int] = a + b match {
+	private def spare_check(a: Int, b: Int, c: Int, tail: List[Int]) : List[Int] = a + b match {
 		case 10 => (10 + c) :: frames(c :: tail)
 		case _  => (a + b) :: frames(c :: tail)
 	}
--- src/test/scala/unit/DefaulScoreCalculatorSpec.scala
+++ src/test/scala/unit/DefaulScoreCalculatorSpec.scala
@@ -24,15 +24,18 @@ class DefaultScoreCalculatorSpec extends WordSpec {
 
 	"frame is a spare" should {
 		"add the first of the next roll" in {
-			calc.calculate(List(8, 2, 4, 2)) should be(20)
+			calc.calculate(List(8, 2, 4)) should be(14)
+		}
+		"except the last frame" in {
+			calc.calculate(List(9, 1, 10)) should be(20)
 		}
 	}
 	
 	"frame is a strike" should {
 		"add both next rolls" in {
-			calc.calculate(List(10, 4, 2)) should be(22)
+			calc.calculate(List(10, 4, 2)) should be(16)
 		}
-		"add both next rolls but not for the last two" in {
+		"except the last frame" in {
 			calc.calculate(List(10, 10, 10)) should be(30)
 		}
 	}
