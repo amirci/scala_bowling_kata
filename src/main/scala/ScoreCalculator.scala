2c0ad289c9e5440f9c3db7f23b036193f615ddea
Amir Barylko <amir@barylko.com>
2011-12-04 20:49:44 -0600

    Added test for default calculator

--- src/main/scala/ScoreCalculator.scala
+++ src/main/scala/ScoreCalculator.scala
@@ -1,13 +1,12 @@
 package mavent.bowlingkata
 
 trait ScoreCalculator {
-	
 	def calculate(rolls: Iterable[Int]) : Int 
 }
 
 class DefaultScoreCalculator extends ScoreCalculator {
 	
 	def calculate(rolls: Iterable[Int]) : Int = {
-		0
+		rolls.reduceLeft[Int](_+_)
 	}
 }
---
+++ src/test/scala/unit/DefaulScoreCalculatorSpec.scala
@@ -0,0 +1,37 @@
+package mavent.bowlingkata.unit
+
+import mavent.bowlingkata._
+
+import org.scalatest._
+import org.scalatest.mock._
+import org.scalatest.matchers.ShouldMatchers._
+
+import org.scalatest.FunSuite
+import org.scalamock.scalatest.MockFactory
+import org.scalamock.ProxyMockFactory
+
+class DefaultScoreCalculatorSpec extends WordSpec {
+
+  "A Default Score Calculator" when {
+
+	val calc = new DefaultScoreCalculator
+	
+    "frame is less than 10" should {
+		"add both rolls" in {
+			calc.calculate(List(2, 6)) should be(8)
+		}
+    }
+
+	"frame is a spare" should {
+		"add the first of the next roll" in {
+			calc.calculate(List(8, 2, 4, 2)) should be(20)
+		}
+	}
+	
+	"frame is a strike" should {
+		"add both next rolls" in {
+			calc.calculate(List(8, 2, 4, 2)) should be(22)
+		}
+	}
+  }
+}
