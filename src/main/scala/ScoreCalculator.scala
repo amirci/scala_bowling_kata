6cd6bb110d6be21cad3d3a6c71a020cf97955e93
Amir Barylko <amir@barylko.com>
2011-12-06 09:18:51 -0600

    Refactored to sum the scores on the first iteration

--- src/main/scala/ScoreCalculator.scala
+++ src/main/scala/ScoreCalculator.scala
@@ -7,23 +7,23 @@ trait ScoreCalculator {
 class DefaultScoreCalculator extends ScoreCalculator {
 	
 	def calculate(rolls: Iterable[Int]) : Int = {
-		frames(rolls.toList) reduce(_+_)
+		frames(rolls.toList)
 	}
 	
-	private def frames(rolls: List[Int]) : List[Int] = rolls match {
-		case first :: second :: Nil => (first + second) :: Nil
-		case first :: second :: third :: Nil => (first + second + third) :: Nil
+	private def frames(rolls: List[Int]) : Int = rolls match {
+		case first :: second :: Nil => first + second
+		case first :: second :: third :: Nil => first + second + third
 		case first :: second :: third :: tail => strike_check(first, second, third, tail)
-		case Nil => Nil
+		case _ => 0
 	}
 
-	private def strike_check(a: Int, b: Int, c: Int, tail: List[Int]) : List[Int] = a match {
-		case 10 => (10 + b + c) :: frames(b :: c :: tail)
-		case _  => spare_check(a, b, c, tail)
+	private def strike_check(a: Int, b: Int, c: Int, tail: List[Int]) : Int = a match {
+		case 10 => 10 + b + c + frames(b :: c :: tail)
+		case _  => spare_check(a, b, c) + frames(c :: tail)
 	}
 	
-	private def spare_check(a: Int, b: Int, c: Int, tail: List[Int]) : List[Int] = a + b match {
-		case 10 => (10 + c) :: frames(c :: tail)
-		case _  => (a + b) :: frames(c :: tail)
+	private def spare_check(a: Int, b: Int, c: Int) : Int = a + b match {
+		case 10 => (10 + c) 
+		case _  => (a + b) 
 	}
 }
